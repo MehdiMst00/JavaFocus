7cb8e5f1e7fef1a588d0954108040cbd71c23dd1
Mohammad Mehdi Mostatabi <mohammadmehdi.mst@outlook.com>
2024-05-02 14:17:40 +0330

    Setup main view design

---
+++ src/main/java/com/mehdimst/javafocus/JavaFocusApplication.java
@@ -0,0 +1,113 @@
+package com.mehdimst.javafocus;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.util.Objects;
+
+public class JavaFocusApplication extends Application {
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        this.stage = stage;
+
+        // Run in background
+        Platform.setImplicitExit(false);
+
+        // Add to system tray
+        javax.swing.SwingUtilities.invokeLater(this::addAppToTray);
+
+        // Load app
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
+        stage.setTitle("Focus on your job");
+        stage.setResizable(false);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass()
+                .getResourceAsStream("icon.png"))));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Sets up a system tray icon for the application.
+     */
+    private void addAppToTray() {
+        try {
+            // ensure awt toolkit is initialized.
+            java.awt.Toolkit.getDefaultToolkit();
+
+            // app requires system tray support, just exit if there is no support.
+            if (!java.awt.SystemTray.isSupported()) {
+                System.out.println("No system tray support, application exiting.");
+                Platform.exit();
+            }
+
+            // set up a system tray icon.
+            java.awt.SystemTray tray = java.awt.SystemTray.getSystemTray();
+
+            java.awt.Image image = Toolkit.getDefaultToolkit().createImage(JavaFocusApplication.class.getResource("icon_tray.png"));
+            java.awt.TrayIcon trayIcon = new java.awt.TrayIcon(image);
+
+            // if the user double-clicks on the tray icon, show the main app stage.
+            trayIcon.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // if the user selects the default menu item (which includes the app name),
+            // show the main app stage.
+            java.awt.MenuItem openItem = new java.awt.MenuItem("Focus on your job");
+            openItem.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // the convention for tray icons seems to be to set the default icon for opening
+            // the application stage in a bold font.
+            java.awt.Font defaultFont = java.awt.Font.decode(null);
+            java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
+            openItem.setFont(boldFont);
+
+            // to really exit the application, the user must go to the system tray icon
+            // and select the exit option, this will shutdown JavaFX and remove the
+            // tray icon (removing the tray icon will also shut down AWT).
+            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
+            exitItem.addActionListener(event -> {
+                Platform.exit();
+                tray.remove(trayIcon);
+            });
+
+            // setup the popup menu for the application.
+            final java.awt.PopupMenu popup = new java.awt.PopupMenu();
+            popup.add(openItem);
+            popup.addSeparator();
+            popup.add(exitItem);
+            trayIcon.setPopupMenu(popup);
+
+            // add the application tray icon to the system tray.
+            tray.add(trayIcon);
+        } catch (java.awt.AWTException e) {
+            System.out.println("Unable to init system tray");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the application stage and ensures that it is brought ot the front
+     * of all stages.
+     */
+    private void showStage() {
+        if (stage != null) {
+            stage.show();
+            stage.toFront();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.mehdimst.javafocus {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+    requires com.jfoenix;
 
     opens com.mehdimst.javafocus to javafx.fxml;
     exports com.mehdimst.javafocus;
